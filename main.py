e95ae8cb90b24265b691d80b655f93b9a7bda16d
Maysha Afrin Munia <maysha.afrin.munia@gmail.com>
2022-10-24 18:21:59 +0600

    Get method finalized

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from typing import Union
-
+import fastapi
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    phone: str
+    password: str
 
 app = FastAPI()
 
+users: list[User] = [
+    User(name="A", email="B", phone="C", password="D"),
+    User(name="a", email="b", phone="c", password="d"),
+]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/users/{name}")
+def get_user(name: str) -> User:
+    for user in users:
+        if user.name == name:
+            return {"name": user.name,
+                    "Email": user.email,
+                    "Phone": user.phone,
+                    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    raise fastapi.HTTPException(status_code=404, detail=f"User not found for name {name}")
