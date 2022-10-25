d285bb43237b04225dd956a463bfa516b8047511
Maysha Afrin Munia <maysha.afrin.munia@gmail.com>
2022-10-25 20:36:40 +0600

    create database, insert user,

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import fastapi
 from fastapi import FastAPI
 from pydantic import BaseModel
+import sqlite3
+
 
 app = FastAPI()
+
 
 class User(BaseModel):
     name: str
@@ -28,11 +31,22 @@ users: list[User] = [
 
 @app.post("/auth/registration/")
 def register_user(reg_req: RegistrationRequest) -> str:
-    if reg_req in users:
-        raise fastapi.HTTPException(status_code=409, detail=f'User with name {reg_req.name} already exists')
-    users.append(reg_req)
+    user = getUserFromRegistrationReq(reg_req)
+    createNewUserInDb(user)
+
     return "User Created!"
 
 @app.post("/auth/login/")
 def login_user(log_req: LoginRequest) -> str:
     return "Login Successful!"
+
+
+
+def getUserFromRegistrationReq(reg_req):
+    user = User(name=reg_req.name,email=reg_req.email,phone=reg_req.phone,password=reg_req.password)
+    return user
+def createNewUserInDb(user):
+    sql_command = "INSERT INTO USER (NAME, EMAIL, PHONE, PASSWORD) VALUES ('" +user.name +"','" + user.email +"','"+ user.phone +"','"+ user.password+"')"
+    conn = sqlite3.connect('db.db')
+    conn.execute(sql_command)
+    conn.commit()
